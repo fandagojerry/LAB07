6911163437bbf9336f6406618380f52079eb9ad0
Jerry <rxiao2@u.rochester.edu>
2018-10-05 11:08:44 -0400

    upate2

---
+++ subpage.php
@@ -0,0 +1,72 @@
+<?php include "inc/html-top.inc"; ?>
+<body>
+<header>
+	<h1>More on the three students</h1>
+</header>
+<?php include "inc/nav.inc"; ?>
+
+	<article>
+	<h2>Victoria Bongard</h2>
+	<h3>Employment</h3>
+	<p>My first official paid job was as a camp counselor and photographer at the Chatham Hall summer camps (held at my high school). I had various volunteer jobs before then, including as a film camp counselor, another camp counselor, a production assistant, and more. This past summer I worked as a paid counselor at Indiana University’s Digital Cinema Academy camp teaching high school students about film and videography.</p>
+	<p>During my second year at college I began three new positions. First, I got the job of a Study Zone Leader, which is a peer academic support position run through the Center for Excellence in Teaching and Learning. My second position was as a paid notetaker, meaning I take notes for any student(s) who may be unable to do so. This position is managed by the Office for Disability Resources. The third position was a Meridian, meaning I give tours and assist the admissions office at the University of Rochester. Though it keeps me busy, I still maintain all three of these positions and just this semester I was promoted to the position of Head Study Zone Leader.</p>
+
+	<h3>Study Spaces</h3>
+	<p>In terms of my academics at the University of Rochester, I have found my study places to be among that which I consider most important to my academic success. I tend to prefer mostly quiet, cozy places to study. In general, I find a loud and crowded space to be too distracting when trying to accomplish work. My favorite study space on campus is Rettner because of the natural lighting, bright colors, and generally quiet atmosphere. It is also very close to much of campus, which is a big bonus.</p>
+	<p>While the new iZone has not been open for long, so far I have found it to be a really nice study area. I also enjoy the Reference Stacks in Rush Rhees, Robinson Library, and the Art and Music Library since both are warm and quiet. I find Carlson, the Stacks, Q and I, and the first floor of Morey to be less conducive to studying, though they are still acceptable places. At the bottom of the totem pole I place Gleason because it is too loud and distracting for me to possibly be productive.</p>
+
+	<h3>Future</h3>
+	<p>The next logical step in my journey is figuring out my post-grad plans. While I still have lots to figure out, and lots of time to do it, I have been thinking recently about what exactly I would like to do and where I would like to be. I know for a fact that I want to live and work somewhere with winters that are noticeably warmer than those in Rochester. I am interested in film, media, web design, public relations, and marketing, which are skills that I think can be combined easily. I would love to work in media marketing and helping companies grow their brand.</p>
+
+
+	<h2>Aliye gallagher</h2>
+	<h3>music</h3>
+      <p>This past summer was affirming that I’ve chosen the right path. I
+      worked at a digital media and PR firm that works with classical musicians. I loved
+      the experience, and worked on a lot of different projects for different artists.
+      I added content (mostly concert dates) to websites, updated and checked up on
+      Facebook pages, worked with photo and video content, updated PR logs and helped
+      to created digital media plans at the quarter. I learned a lot about the
+      classical music industry, and that I really liked working with clients in the arts.
+      After graduation, I plan to pursue working on digital media for arts-based clients.
+     </p>
+     <h3>film</h3>
+     <p>Last fall, I studied abroad in London, and I had an internship at a firm
+      that interviews film stars and filmmakers for promo for films, and edits
+      them into YouTube videos. I had the chance to interview stars, directors,
+      and producers for a film festival in junket setting (sitting down in a room
+      with the movie poster behind you), as well as twice on the red carpet. I
+      learned Premiere Pro like the back of my hand, and went from being the most
+      awkward as an interviewer to only slightly awkward.
+     </p>
+
+     <h3>photography</h3>
+     <p>I also love photography and the arts, and am a digital photographer, though
+      I love working with film photography as well. My sophomore year, I took an
+      Art History class about early photography, and we took frequent trips to the
+      Eastman Museum as part of the course. For a final project in that class,
+      I studied one of the nine remaining Daugerreotypes (the first form of permanent
+      photography) of Fredrick Douglass, which was on loan at the Rare Books and
+      Special Collections library at the time. I hope at some point in my career,
+      I’ll be able to combine my passion for photography and the arts and my work
+      in Digital Media.
+    </p>
+
+     <h2>Li Guo</h2>
+	<h3>Work Experience</h3>
+	<p>The summer after freshman year, I worked for a Public Relations firm. It involved making a lot of cold calls, which I hated, but really got me over my distaste of talking on the phone. I also prepared press kits and got to write media pitches, which was considerably easier for me to do. The next summer, I worked at a not-for-profit called DoSomething.org! They were in the business of creating social good by encouraging teenagers to take action on various issues ranging from environmental awareness to sexual health. I worked on the Marketing team, and that internship basically locked me into wanting to work in Marketing, as well as for an organization that dealt with something that I was really passionate about. It was really easy waking up at 7AM to go to work when I was happy to be there. The company culture was really fun, with themed Fridays, bake-offs, and pool floats everywhere. Now that I’m a senior, I’m starting the job hunt for post-graduation, so here’s hoping that I land a position with a company that cares about its employees!</p>
+	
+	<h3>Early Education</h3>
+	<p>When I started elementary school, I didn’t speak much English, so I had to take ESL classes. I picked it up quickly, and started to love reading, which probably helped. I ended up testing into a magnet middle school for music, so I spent three years pretending I knew how to play the cello. The middle school had an advanced curriculum, which prepared me well for the SHSAT, a standardized exam New Yorkers take to gain admission into selective high schools. I scored pretty high, and went to the “#1 public high school in NYC”. It was super competitive, but they offered a wide variety of courses, like Organic Chemistry.</p>
+
+	<h3>College Education</h3>
+	<p>Taking Orgo in high school convinced me I wanted to work with Chemistry for the rest of my life, so I applied to way too many colleges as a Chemical Engineering major. Fast forward eight months, and I wound up at the University of Rochester. I took my introductory engineering courses and realized I did not want to be in a lab for the rest of my life. I did take a super interesting Economics course, and changed my major to Business by the end of my freshman year. It was one of the better decisions I’ve made, but I still missed using the more computational part of my brain, so I decided to give Computer Science another shot (I had taken a few classes in high school), and turned that into a minor.</p>
+
+</article>
+<footer>
+	<p>Instagram: @bongard.jpg</p>
+</footer>
+<script src="http://code.jquery.com/jquery.js"></script>
+<script src="js/menu-highlighter.js"></script>
+</body>
+</html>
