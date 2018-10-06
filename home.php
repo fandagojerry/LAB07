fccbdd1118091919a15244dccafe3b368143d075
Xinyi <xchen92@u.rochester.edu>
2018-10-07 01:43:16 +0800

    hero

--- home.php
+++ home.php
@@ -1,46 +1,35 @@
 <?php include "inc/html.inc"; ?>
-<header>
-      <h1> Three students at UR</h1>
-</header>
+
+
+<div class="hero-image">
+   <h1> Three students at UR</h1>
+</div>
+
+
 
 <article>
 	<h2>Victoria Bongard</h2>
-	<img src="images/victoria.jpg" alt="Victoria">
-	<h3>About Me</h3>
-	
-	<p>My name is Victoria Bongard and I am a junior attending the University of Rochester. I am double majoring in Film and Media Studies and Digital Media Studies with a minor in Computer Science. I was born in Durham, North Carolina and then spent a year of my infancy in New York City before moving to Blacksburg, Virginia for most of the remainder of my childhood. My family moved to Bloomington, Indiana during the summer before I started high school. Two months after moving I began attending Chatham Hall, an all-girls boarding school located in rural southern Virginia. While my family has lived in Indiana for the past 6 years, I have only ever spent breaks there. Thus, I consider myself to be from Virginia and occasionally Indiana, too.</p>
+  <img class="firpic" src="images/victoria.jpg" alt="Victoria">	
+	<p>My name is Victoria Bongard and I am a junior attending the University of Rochester. I am double majoring in Film and Media Studies and Digital Media Studies with a minor in Computer Science. I was born in Durham, North Carolina and then spent a year of my infancy in New York City before moving to Blacksburg, Virginia for most of the remainder of my childhood. </p>
+
 
 	<h2>Aliye gallagher</h2>
-	<img class="secpic" src="images/aliye.jpg" alt="Aliye">
-	<h3>About me</h3>
-	<p>I created my own major here at the U of R (a DIY major!), through the interdepartmental
-       major track. I realized in my first year that none of the majors that were offered were
-       exactly right for me, and started to look into the interdepartmental major option.
-       Basically, you're petitioning the school that none of the majors would fulfil what
-       you want to do. My sophomore year, I began the process of designing my major, by
-       having a lot of meetings with the Multidisciplinary Studies office, talking to
-       many professors that I had taken classes with that I liked, and attempting to
-       create a solid way of describing the major that I wanted to create. After a semester
-       of hard work, I had two advisors sign on with me and a four page proposal,
-       answering: 1. What my future plans after graduating with said major would be,
-       2. Why I couldn't declare a major that is already established, and 3. A detailed
-       explanation of each class I planned to take and how they fit in. A few months later,
-       my proposal was approved.
+  <img class="secpic" src="images/aliye.jpg" alt="Aliye">
+  <p>I created my own major here at the U of R (a DIY major!), through the interdepartmental major track. I realized in my first year that none of the majors that were offered were exactly right for me, and started to look into the interdepartmental major option.
      </p>
      
-    <h2>Lihui Guo</h2>
+  <h2>Lihui Guo</h2>
     <img src="images/lihui.png" alt="Lihui">
-    <h3>About me</h3>
-	<p>I grew up in Brooklyn, New York in a predominantly Asian-American and Italian-American neighborhood. My parents had just moved from Japan/China to be with other family in New York City. I spent a lot of time with relatives as a child, particularly with my older cousins and grandparents. Our neighborhood had everything we needed – everything was within walking distance, and it was really nice to have family so close. My family was and still is super important to me. They’re always my biggest supporters and I really appreciate how my parents aren’t stereotypical “Asian tiger parents”.</p>
+	<p>I grew up in Brooklyn, New York in a predominantly Asian-American and Italian-American neighborhood. My parents had just moved from Japan/China to be with other family in New York City. I spent a lot of time with relatives as a child, particularly with my older cousins and grandparents.</p>
 
 </article>
 <footer>
 	<p> CSC 174 Team TOKYO </p>
 
 	 <ul> 
+        <li> <a href="http://csc174.org/assignment02/vbongard/">Victoria's page</a></li>
         <li> <a href="http://csc174.org/assignment02/agallagher/">Aliye's page</a></li> 
-        <li> <a href="http://csc174.org/assignment02/lguo/">Lihui's page</a> </li> 
-        <li> <a href="http://csc174.org/assignment02/vbongard/">Victoria's page</a></li> 
+        <li> <a href="http://csc174.org/assignment02/lguo/">Lihui's page</a> </li>
     </ul>
 </footer>
 </div>
