19bcfec08115934c8a5f6371bed4eec3d51abd6f
Xinyi <xchen92@u.rochester.edu>
2018-10-21 02:50:11 +0800

    Home page done

--- index.php
+++ index.php
@@ -15,27 +15,27 @@
   <div class="grid1">
 	<h2>Victoria Bongard</h2>
   <img src="images/victoria.jpg" alt="Victoria">	
-	<p>I am double majoring in FMS and DMS. My first official paid job was as a camp counselor and photographer at the Chatham Hall summer camps.</p>
+	<p>I double major in FMS and DMS. My first official paid job was a photographer at the Chatham Hall summer camps.</p>
  </div>
 
   <div class="grid2">
 	<h2>Aliye Gallagher</h2>
   <img src="images/aliye.jpg" alt="Aliye">
-  <p>I created my own major here(a DIY major) through the interdepartmental major track. I love photography, and I am a digital photographer.</p>
+  <p>I created my own major through the interdepartmental major track. I love photography. I am a digital photographer.</p>
   </div> 
 
   <div class="grid3">
   <h2>Lihui Guo</h2>
     <img src="images/lihui.png" alt="Lihui">
 	<p>I am a Senior at the University of Rochester Majoring in Business- Marketing.
-</p>
+  </p>
   </div> 
 
 </section>
 
-<div class="bottomright">
-<ul class="action"><li><a href="subpage.php"> read more! </a></li></ul>
-</div>
+<section class="bottomright">
+<ul><li><a href="subpage.php"> Read more! </a></li></ul>
+</section>
 
 </div>
 </body>
