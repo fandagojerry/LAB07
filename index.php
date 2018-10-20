e8a9610d72c7b7f74f8a28bdb45931148e6003a1
Xinyi <xchen92@u.rochester.edu>
2018-10-21 02:31:55 +0800

    002

--- index.php
+++ index.php
@@ -1,29 +1,35 @@
 <?php include "inc/html.inc"; ?>
+
 <div class="topleft">
 <img src="images/logo.png" alt="LOGO">
 </div>
-
 <?php include "inc/nav.inc"; ?>
 
-<div class="middle">
-   <h1> Three students at UR</h1>
+
+<div class="middle hero">
+   <h1> Three UR Students</h1>
 </div>
 
 
-
 <section class="bottomleft">
+  <div class="grid1">
 	<h2>Victoria Bongard</h2>
-  <img class="firpic" src="images/victoria.jpg" alt="Victoria">	
-	<p>My name is Victoria Bongard and I am a junior attending the University of Rochester. I am double majoring in Film and Media Studies and Digital Media Studies with a minor in Computer Science. I was born in Durham, North Carolina and then spent a year of my infancy in New York City before moving to Blacksburg, Virginia for most of the remainder of my childhood. </p>
+  <img src="images/victoria.jpg" alt="Victoria">	
+	<p>I am double majoring in FMS and DMS. My first official paid job was as a camp counselor and photographer at the Chatham Hall summer camps.</p>
+ </div>
 
-
+  <div class="grid2">
 	<h2>Aliye Gallagher</h2>
-  <img class="secpic" src="images/aliye.jpg" alt="Aliye">
-  <p>I created my own major here at the U of R (a DIY major!), through the interdepartmental major track. I realized in my first year that none of the majors that were offered were exactly right for me, and started to look into the interdepartmental major option. Basically, you're petitioning the school that none of the majors would fulfil what you want to do. </p>
-     
+  <img src="images/aliye.jpg" alt="Aliye">
+  <p>I created my own major here(a DIY major) through the interdepartmental major track. I love photography, and I am a digital photographer.</p>
+  </div> 
+
+  <div class="grid3">
   <h2>Lihui Guo</h2>
     <img src="images/lihui.png" alt="Lihui">
-	<p>I grew up in Brooklyn, New York in a predominantly Asian-American and Italian-American neighborhood. My parents had just moved from Japan/China to be with other family in New York City. I spent a lot of time with relatives as a child, particularly with my older cousins and grandparents.</p>
+	<p>I am a Senior at the University of Rochester Majoring in Business- Marketing.
+</p>
+  </div> 
 
 </section>
 
