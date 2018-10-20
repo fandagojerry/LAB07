0eb914de589adf1878929e72ab51c03eaa287d7a
Jerry <rxiao2@u.rochester.edu>
2018-10-20 01:49:50 -0400

    123

--- subpage.php
+++ subpage.php
@@ -1,16 +1,25 @@
 <?php include "inc/html.inc"; ?>
+
+<div class="container2">
 <header>
-      <h1>Experence in Art VS Marketing Experience</h1>
+      <h1>Experience in Art VS Marketing Experience</h1>
 </header>
 
 
-	<article>
+
+
+  <section class="lead">
+  <h2> About the three students</h2>
+  <p>Hey! This website will tell some brief stories about three U of R students: Victoria Bongard, Aliye Gallagher,and Li Guo. Two students have similarities in film and photography experience. The third student is different in work experience in marketing than the other two. Can you find them? </p>
+  </section>
+
+  <article>
 
 	<h2>Victoria Bongard</h2>
 	<h3>Employment</h3>
 	<p>My first official paid job was as a camp counselor and photographer at the Chatham Hall summer camps (held at my high school). I had various volunteer jobs before then, including as a film camp counselor, another camp counselor, a production assistant, and more. This past summer I worked as a paid counselor at Indiana University’s Digital Cinema Academy camp teaching high school students about film and videography.</p>
 	
-	<h2>Aliye gallagher</h2>    
+	<h2>Aliye Gallagher</h2>    
      <h3>film</h3>
      <p>Last fall, I studied abroad in London, and I had an internship at a firm
       that interviews film stars and filmmakers for promo for films, and edits
@@ -19,7 +28,7 @@
       with the movie poster behind you), as well as twice on the red carpet. I
       learned Premiere Pro like the back of my hand, and went from being the most
       awkward as an interviewer to only slightly awkward.
-     </p>
+      </p>
 
      <h3>photography</h3>
      <p>I also love photography and the arts, and am a digital photographer, though
@@ -31,22 +40,65 @@
       Special Collections library at the time. I hope at some point in my career,
       I’ll be able to combine my passion for photography and the arts and my work
       in Digital Media.
-    </p>
+      </p>
 
     <h2>Li Guo</h2>
 	<h3>Work Experience</h3>
 	<p>The summer after freshman year, I worked for a Public Relations firm. It involved making a lot of cold calls, which I hated, but really got me over my distaste of talking on the phone. I also prepared press kits and got to write media pitches, which was considerably easier for me to do. The next summer, I worked at a not-for-profit called DoSomething.org! They were in the business of creating social good by encouraging teenagers to take action on various issues ranging from environmental awareness to sexual health. I worked on the Marketing team, and that internship basically locked me into wanting to work in Marketing, as well as for an organization that dealt with something that I was really passionate about. It was really easy waking up at 7AM to go to work when I was happy to be there. The company culture was really fun, with themed Fridays, bake-offs, and pool floats everywhere. Now that I’m a senior, I’m starting the job hunt for post-graduation, so here’s hoping that I land a position with a company that cares about its employees!</p>
 	
 </article>
-<footer>
-	<p> CSC 174 Team TOKYO </p>
 
-	 <ul> 
+<aside>
+
+<h2>Clues</h2>
+ <ol>
+    <li> Victoria
+         <ul>
+          <li>camp counselor</li>
+          <li>photographer</li>
+          <li>interviewer</li>
+          <li>Indiana University</li>
+          <li>film and videography</li>
+        </ul>
+    </li>
+
+    <li> Aliye
+         <ul>
+          <li>digital photographer</li>
+          <li>film photography</li>
+          <li>YouTube</li>
+          <li>Premiere Pro</li>
+          <li>Daugerreotypes</li>
+        </ul>
+    </li>
+
+    <li> Li
+         <ul>
+          <li>Public Relations firm</li>
+          <li>press kits</li>
+          <li>media pitches</li>
+          <li>DoSomething.org</li>
+          <li>post graduation</li>
+        </ul>
+    </li>
+  </ol>
+
+</aside>
+
+
+<footer>
+	<div> CSC 174 Team TOKYO </div>
+
+<ul> 
+        <li> <a href="http://csc174.org/assignment02/vbongard/">Victoria's page</a></li>
         <li> <a href="http://csc174.org/assignment02/agallagher/">Aliye's page</a></li> 
-        <li> <a href="http://csc174.org/assignment02/lguo/">Lihui's page</a> </li> 
-        <li> <a href="http://csc174.org/assignment02/vbongard/">Victoria's page</a></li> 
+        <li> <a href="http://csc174.org/assignment02/lguo/">Lihui's page</a> </li>
     </ul>
+
+
+     <ul class="action"><li><a href="index.php"> Back to Home</a></li></ul>
 </footer>
+</div>
 </div>
 </body>
 </html>
