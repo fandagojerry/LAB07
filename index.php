4ce9d2426d7eadfe751e1b02c54c7ce328e682c2
Xinyi <xchen92@u.rochester.edu>
2018-10-21 05:47:40 +0800

    I am SOOO TIRED :astonished:

--- index.php
+++ index.php
@@ -13,28 +13,25 @@
 
 <section class="bottomleft">
   <div class="grid1">
-	<h2>Victoria Bongard</h2>
-  <img src="images/victoria.jpg" alt="Victoria">	
+	<h2>Victoria Bongard</h2>	
 	<p>I double major in FMS and DMS. My first official paid job was a photographer at the Chatham Hall summer camps.</p>
  </div>
 
   <div class="grid2">
 	<h2>Aliye Gallagher</h2>
-  <img src="images/aliye.jpg" alt="Aliye">
   <p>I created my own major through the interdepartmental major track. I love photography. I am a digital photographer.</p>
   </div> 
 
   <div class="grid3">
   <h2>Lihui Guo</h2>
-    <img src="images/lihui.png" alt="Lihui">
 	<p>I am a Senior at the University of Rochester Majoring in Business- Marketing.
   </p>
   </div> 
 
-</section>
-
-<section class="bottomright">
+<div class="bottomright grid4">
 <ul><li><a href="subpage.php"> Read more! </a></li></ul>
+</div>
+
 </section>
 
 </div>
--- subpage.php
+++ subpage.php
@@ -10,13 +10,14 @@
   <p>Victoria Bongard and Aliye Gallagher have similarities in film and photography experience. Li Guo is in marketing and business field</p>
   </section>
 
-<div class="wrapper">
   <section class="content">
-	<h2>Victoria Bongard</h2>
-	<h3>Employment</h3>
-	<p>My first official paid job was as a camp counselor and photographer at the Chatham Hall summer camps (held at my high school). I had various volunteer jobs before then, including as a film camp counselor, another camp counselor, a production assistant, and more. This past summer I worked as a paid counselor at Indiana University’s Digital Cinema Academy camp teaching high school students about film and videography.</p>
+	<h2 class="bgyellow">Victoria Bongard</h2>
+ <img class="gridr2" src="images/victoria.jpg" alt="Victoria">
+	<h3 class="gridr2">Employment</h3>
+	<p class="gridr2">My first official paid job was as a camp counselor and photographer at the Chatham Hall summer camps (held at my high school). I had various volunteer jobs before then, including as a film camp counselor, another camp counselor, a production assistant, and more. This past summer I worked as a paid counselor at Indiana University’s Digital Cinema Academy camp teaching high school students about film and videography.</p>
 	
-	<h2>Aliye Gallagher</h2>    
+	<h2 class="bgyellow">Aliye Gallagher</h2>  
+      <img class="gridr4" src="images/aliye.jpg" alt="Aliye">  
      <h3>film</h3>
      <p>Last fall, I studied abroad in London, and I had an internship at a firm
       that interviews film stars and filmmakers for promo for films, and edits
@@ -39,44 +40,15 @@
       in Digital Media.
       </p>
 
-    <h2>Li Guo</h2>
+    <h2 class="bgwhite">Li Guo</h2>
+   <img class="border-dot gridr7" src="images/lihui.png" alt="Lihui">
 	<h3>Work Experience</h3>
-	<p>The summer after freshman year, I worked for a Public Relations firm. It involved making a lot of cold calls, which I hated, but really got me over my distaste of talking on the phone. I also prepared press kits and got to write media pitches, which was considerably easier for me to do. The next summer, I worked at a not-for-profit called DoSomething.org! They were in the business of creating social good by encouraging teenagers to take action on various issues ranging from environmental awareness to sexual health. I worked on the Marketing team, and that internship basically locked me into wanting to work in Marketing, as well as for an organization that dealt with something that I was really passionate about. It was really easy waking up at 7AM to go to work when I was happy to be there. The company culture was really fun, with themed Fridays, bake-offs, and pool floats everywhere. Now that I’m a senior, I’m starting the job hunt for post-graduation, so here’s hoping that I land a position with a company that cares about its employees!</p>
+	<p>The summer after freshman year, I worked for a Public Relations firm. It involved making a lot of cold calls, which I hated, but really got me over my distaste of talking on the phone. I also prepared press kits and got to write media pitches, which was considerably easier for me to do. </p>
+
+  <h3>DoSomething.org</h3>
+  <p> The next summer, I worked at a not-for-profit called DoSomething.org! They were in the business of creating social good by encouraging teenagers to take action on various issues ranging from environmental awareness to sexual health. I worked on the Marketing team, and that internship basically locked me into wanting to work in Marketing, as well as for an organization that dealt with something that I was really passionate about. It was really easy waking up at 7AM to go to work when I was happy to be there. The company culture was really fun, with themed Fridays, bake-offs, and pool floats everywhere. Now that I’m a senior, I’m starting the job hunt for post-graduation, so here’s hoping that I land a position with a company that cares about its employees!</p>
 	
 </section>
-
-<aside>
-<h2>Clues</h2>
-    <h3> Victoria</h3>
-         <ul>
-          <li>camp counselor</li>
-          <li>photographer</li>
-          <li>interviewer</li>
-          <li>Indiana University</li>
-          <li>film and videography</li>
-        </ul>
-
-    <h3> Aliye</h3>
-         <ul>
-          <li>digital photographer</li>
-          <li>film photography</li>
-          <li>YouTube</li>
-          <li>Premiere Pro</li>
-          <li>Daugerreotypes</li>
-        </ul>
-
-    <h3> Li </h3>
-         <ul>
-          <li>Public Relations firm</li>
-          <li>press kits</li>
-          <li>media pitches</li>
-          <li>DoSomething.org</li>
-          <li>post graduation</li>
-        </ul>
-
-</aside>
-
-
 <footer>
 	<div> CSC 174 Team TOKYO </div>
 
@@ -86,7 +58,6 @@
         <li> <a href="http://csc174.org/assignment02/lguo/">Lihui's page</a> </li>
 </ul>
 </footer>
-</div>
 </div>
 
 </body>
